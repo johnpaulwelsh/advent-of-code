02c2f9d2c0a67200d1aad34aa113c4651b6165dd
johnpaulwelsh <john.welsh1@marist.edu>
2022-12-28 08:02:25 -0500

    Day 10 part 2 passing

--- src/aoc2022/main/Day10.java
+++ src/aoc2022/main/Day10.java
@@ -1,12 +1,29 @@
 package src.aoc2022.main;
 
-import src.aoc2022.main.Pojos.CpuInstructionDuringOneCycle;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Day10 {
+
+    static class CpuInstructionDuringOneCycle {
+        String instructionName;
+        int registerAdditionValue;
+
+        public CpuInstructionDuringOneCycle(String instructionName, int registerAdditionValue) {
+            this.instructionName = instructionName;
+            this.registerAdditionValue = registerAdditionValue;
+        }
+
+        public int getRegisterAdditionValue() {
+            return registerAdditionValue;
+        }
+    }
+
+    static final int CRT_WIDTH = 40;
+    static final int CRT_HEIGHT = 6;
+    static final String DARK = ".";
+    static final String LIT = "#";
 
     static List<Integer> cyclesToCheck = Arrays.asList(20, 60, 100, 140, 180, 220);
 
@@ -26,12 +43,7 @@ public class Day10 {
         }
     }
 
-    public static void main(String[] args) {
-        List<String> list = Utilities.readFileInList(
-                "/Users/johnpaulwelsh/Documents/advent-of-code/src/aoc2022/resources/day10input.txt");
-
-        populateInstructions(list);
-
+    public static List<Integer> findSignalStrengths() {
         List<Integer> signalStrengths = new ArrayList<>();
         int register = 1;
         for (int cycleCounter = 1; cycleCounter <= instructionsDuringOneCycle.size(); cycleCounter++) {
@@ -42,7 +54,50 @@ public class Day10 {
             }
             register += instAddVal;
         }
+        return signalStrengths;
+    }
+
+    public static String[][] createAndDrawCrtScreen() {
+        String[][] crtDisplay = new String[CRT_HEIGHT][CRT_WIDTH];
+        for (String[] strings : crtDisplay) {
+            Arrays.fill(strings, DARK);
+        }
+
+        int register = 1;
+        for (int cycleCounter = 1; cycleCounter <= instructionsDuringOneCycle.size(); cycleCounter++) {
+            CpuInstructionDuringOneCycle inst = instructionsDuringOneCycle.get(cycleCounter-1);
+            int instAddVal = inst.getRegisterAdditionValue();
+
+            int registerMod = register % CRT_WIDTH;
+            List<Integer> pixelPositions = Arrays.asList(registerMod, registerMod-1, registerMod+1);
+            int cycleCounterMod = (cycleCounter-1) % CRT_WIDTH;
+
+            if (pixelPositions.contains(cycleCounterMod)) {
+                int row = (cycleCounter-1) / CRT_WIDTH; // 0 - 5
+                int column = (cycleCounter-1) % CRT_WIDTH; // 0 - 39
+                crtDisplay[row][column] = LIT;
+            }
+
+            register += instAddVal;
+        }
+
+        return crtDisplay;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = Utilities.readFileInList(
+                "/Users/johnpaulwelsh/Documents/advent-of-code/src/aoc2022/resources/day10input.txt");
+
+        populateInstructions(list);
+
+        List<Integer> signalStrengths = findSignalStrengths();
 
         System.out.println("answer 1 = " + signalStrengths.stream().mapToInt(i -> i).sum());
+
+        System.out.println("answer 2 = ");
+        String[][] crtDisplay = createAndDrawCrtScreen();
+        for (String[] strings : crtDisplay) {
+            System.out.println(String.join("", strings));
+        }
     }
 }
